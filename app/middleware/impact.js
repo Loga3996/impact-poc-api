a440a2ca3bbbbf022e7319d9d2ba1e971a22c7b8
Loganathan <loganathan.v@congruentglobal.com>
2019-08-12 19:15:11 +0530

    changed coding based on year,company,month selection

--- app/middleware/impact.js
+++ app/middleware/impact.js
@@ -3,54 +3,37 @@ import Cart from '../models/cart-orders';
 const companyAmountDetails = (req, callback) => {
     let year = req.body.year;
     let code = req.body.code;
+    let mcode = req.body.mcode;
+    console.log(year);
     switch (req.params.apiType) {
-        case 'YM':
+        case 'Ym':
             if (year) {
+                Cart.SelectCompanyValues(year, (err, data) => {
+                    if (err) return callback(err);
+                    callback(null, data);
+                })
+            } else {
+                Cart.SelectYearValues((err, data) => {
+                    if (err) return callback(err);
+                    callback(null, data);
+                })
+            }
+            break;
+        case 'Cart_Orders':
+            if (year && code && mcode) {
+                Cart.MonthWiseOfficeAmountBasedOnCompanyInSpecYear({ year, code, mcode }, (err, data) => {
+                    if (err) return callback(err);
+                    callback(null, data);
+                })
+            }
+            else if (year && code) {
+                Cart.MonthWiseCompanyAmountBasedOnYear({ year, code }, (err, data) => {
+                    if (err) return callback(err);
+                    callback(null, data);
+                })
+            }
+            else if (year) {
                 Cart.MonthWiseRevenueBasedOnYear(year, (err, data) => {
-                    if (err) return callback(err);
-                    callback(null, data);
-                })
-            } else {
-                Cart.YearWiseRevenue((err, data) => {
-                    if (err) return callback(err);
-                    callback(null, data);
-                })
-            }
-            break;
-        case 'Company':
-            if (year && code) {
-                Cart.MonthWiseCompanyAmountBasedOnCompany({ year, code }, (err, data) => {
-                    if (err) return callback(err);
-                    callback(null, data);
-                })
-            }
-            else if (year) {
-                Cart.CompanyWiseRevenueBasedOnYear(year, (err, data) => {
-                    if (err) return callback(err);
-                    callback(null, data);
-                })
-            } else {
-                Cart.CompanyWiseRevenue((err, data) => {
-                    if (err) return callback(err);
-                    callback(null, data);
-                })
-            }
-            break;
-        case 'Office':
-            if (year && code) {
-                Cart.OfficeWiseRevenueBasedOnCompanyInSpecficYear({ year, code }, (err, data) => {
-                    if (err) return callback(err);
-                    callback(null, data);
-                })
-            }
-            else if (code) {
-                Cart.OfficeWiseRevenueBasedOnCompany(code, (err, data) => {
-                    if (err) return callback(err);
-                    callback(null, data);
-                })
-            }
-            else if (year) {
-                Cart.OfficeWiseRevenueBasedOnSpecficYear(year, (err, data) => {
                     if (err) return callback(err);
                     callback(null, data);
                 })
--- app/models/cart-orders.js
+++ app/models/cart-orders.js
@@ -1,85 +1,62 @@
 
 import sql from '../db/dataBase';
 
-const yearWiseRevenue = (result) => {
-    let sql_query = `select distinct year(ordered_at) as date,sum(total_amount) as amount from 
-        cart_orders where ordered_at != 0 group by year(ordered_at)`;
-    sql.query(sql_query, result);
+const selectYearValues = (result) => {
+    let sql_query = `select distinct year(ordered_at) as year from cart_orders where ordered_at !=0`;
+    sql.query(sql_query, result)
+}
+const selectCompanyValues = (year,result) => {
+    let sql_query = `select company,code from
+    (select concat(rc.Number,"-" ,rc.COMPANY_NAME) as company,rc.Number as code from 
+    cart_orders as c join RMS_IMPACT_COMPANY as rc on c.COMPANY=rc.NUMBER 
+    where year(ordered_at)=${year})
+    as selection group by company,code;`;
+    sql.query(sql_query,year, result)
 }
 
 const monthWiseRevenueBasedOnYear = (year, result) => {
-    let sql_query = `select DATE_FORMAT(ordered_at,'%b-%Y') as month ,sum(total_amount) as amount from 
-        cart_orders where year(ordered_at)=${year} group by month`;
+    let sql_query = `select DATE_FORMAT(ordered_at,'%b-%Y') as months,month(ordered_at) as mcode ,sum(total_amount) as amount from 
+    cart_orders where year(ordered_at)=${year} group by months,mcode order by mcode`;
     sql.query(sql_query, year, result)
 }
 
-const companyWiseRevenue = (result) => {
-    let sql_query = `select company ,sum(amount) as amount,code from
-    (select c.total_amount as amount,rc.COMPANY_NAME as company, rc.Number as code from 
-    cart_orders as c join RMS_IMPACT_COMPANY as rc on c.COMPANY=rc.NUMBER)
-    as selection group by company,code;`;
-    sql.query(sql_query, result);
-}
+// const monthWiseCompanyAmountBasedOnYear = ({ year, company }, result) => {
+//     let sql_query = `select months,sum(amount) as amount,company,mcode,year,code from
+//     (select DATE_FORMAT(ordered_at,'%b-%Y') as months,year(ordered_at)as year,month(ordered_at) as mcode, c.company as code ,c.total_amount as amount,rc.COMPANY_NAME as company from 
+//     cart_orders as c join RMS_IMPACT_COMPANY as rc on c.COMPANY=rc.NUMBER 
+//     where year(ordered_at)=${year} and rc.COMPANY_NAME='${company}')
+//     as selection group by company,months,mcode,year,code order by mcode;`;
+    
+//     console.log("sql", sql_query)
+//     sql.query(sql_query, { year, company }, result);
+// }
 
-const companyWiseRevenueBasedOnYear = (year, result) => {
-    let sql_query = `select date,company,sum(amount) as amount,code from
-        (select year(c.ordered_at) as date, c.total_amount as amount,rc.COMPANY_NAME as company,rc.Number as code from 
-        cart_orders as c join RMS_IMPACT_COMPANY as rc on c.COMPANY=rc.NUMBER 
-        where year(ordered_at)=${year})
-        as selection group by company,code,date;`;
-    sql.query(sql_query, year, result);
-}
-
-const monthWiseCompanyAmountBasedOnCompany = ({ year, code }, result) => {
-    let sql_query = `select month,sum(amount) as amount,company from
-        (select DATE_FORMAT(ordered_at,'%b-%Y') as month,c.total_amount as amount,rc.COMPANY_NAME as company from 
-        cart_orders as c join RMS_IMPACT_COMPANY as rc on c.COMPANY=rc.NUMBER 
-        where year(ordered_at)=${year} and c.COMPANY=${code})
-        as selection group by company,month;`;
+const monthWiseCompanyAmountBasedOnYear = ({ year, code }, result) => {
+    let sql_query = `select months,sum(amount) as amount,company,mcode,year,code from
+    (select DATE_FORMAT(ordered_at,'%b-%Y') as months,year(ordered_at)as year,month(ordered_at) as mcode, c.company as code ,c.total_amount as amount,rc.COMPANY_NAME as company from 
+    cart_orders as c join RMS_IMPACT_COMPANY as rc on c.COMPANY=rc.NUMBER 
+    where year(ordered_at)=${year} and rc.NUMBER=${code})
+    as selection group by company,months,mcode,year,code order by mcode;`;
     sql.query(sql_query, { year, code }, result);
 }
 
-const officeWiseRevenueBasedOnCompany = (code, result) => {
-    let sql_query = `select office,sum(Amount) as amount,company from
-        (select DATE_FORMAT(c.ordered_at,'%Y-%m-%d') as orderdate,c.total_amount as Amount,
-        r.OFFICE_NAME as office,rc.COMPANY_NAME as company from cart_orders as c 
-        join RMS_OFFICE as r on c.OFFICE = r.NUMBER 
-        join RMS_IMPACT_COMPANY as rc on c.COMPANY=rc.NUMBER 
-        where rc.NUMBER=${code} and c.total_amount !=0)
-        as selection group by company,office;`;
-    sql.query(sql_query, code, result);
-}
-const officeWiseRevenueBasedOnSpecficYear = (year, result) => {
-    let sql_query = `select office,sum(Amount) as amount,date,code from
-        (select year(c.ordered_at) as date,c.total_amount as Amount,
-        r.OFFICE_NAME as office,r.NUMBER as code,rc.COMPANY_NAME as company from cart_orders as c 
-        join RMS_OFFICE as r on c.OFFICE = r.NUMBER 
-        join RMS_IMPACT_COMPANY as rc on c.COMPANY=rc.NUMBER 
-        where year(ordered_at)=${year} and c.total_amount !=0)
-        as selection group by company,office,date,code;`;
-    sql.query(sql_query, year, result);
-}
-
-const officeWiseRevenueBasedOnCompanyInSpecficYear = ({ year, code }, result) => {
-    let sql_query = `select office,sum(Amount) as amount,month,code from
-        (select DATE_FORMAT(c.ordered_at,'%b-%Y') as month,c.total_amount as Amount,
-        r.OFFICE_NAME as office, r.NUMBER as code,rc.COMPANY_NAME as company from cart_orders as c 
-        join RMS_OFFICE as r on c.OFFICE = r.NUMBER 
-        join RMS_IMPACT_COMPANY as rc on c.COMPANY=rc.NUMBER 
-        where year(ordered_at)=${year} and r.NUMBER=${code} and c.total_amount !=0)
-        as selection group by company,office,month,code;`;
-    sql.query(sql_query, { year, code }, result);
+const monthWiseOfficeAmountBasedOnCompanyInSpecYear = ({ year, code ,mcode}, result) => {
+    let sql_query = `select office,sum(Amount) as amount,code from
+    (select c.total_amount as Amount,
+    CONCAT(r.OFFICE_NAME,"-",r.NUMBER) as office, r.NUMBER as code,rc.COMPANY_NAME as company from cart_orders as c 
+    join RMS_OFFICE as r on c.OFFICE = r.NUMBER 
+    join RMS_IMPACT_COMPANY as rc on c.COMPANY=rc.NUMBER 
+    where year(ordered_at)=${year} and rc.NUMBER =${code} and month(ordered_at)=${mcode})
+    as selection group by company,office,code`;
+    sql.query(sql_query, { year, code,mcode }, result);
 }
 
 module.exports = (() => {
     return {
-        'YearWiseRevenue': yearWiseRevenue,
+        'SelectYearValues':selectYearValues,
+        'SelectCompanyValues':selectCompanyValues,
         'MonthWiseRevenueBasedOnYear': monthWiseRevenueBasedOnYear,
-        'CompanyWiseRevenue': companyWiseRevenue,
-        'CompanyWiseRevenueBasedOnYear': companyWiseRevenueBasedOnYear,
-        'MonthWiseCompanyAmountBasedOnCompany': monthWiseCompanyAmountBasedOnCompany,
-        'OfficeWiseRevenueBasedOnCompany': officeWiseRevenueBasedOnCompany,
-        'OfficeWiseRevenueBasedOnSpecficYear': officeWiseRevenueBasedOnSpecficYear,
-        'OfficeWiseRevenueBasedOnCompanyInSpecficYear': officeWiseRevenueBasedOnCompanyInSpecficYear
+        'MonthWiseCompanyAmountBasedOnYear': monthWiseCompanyAmountBasedOnYear,
+        'MonthWiseOfficeAmountBasedOnCompanyInSpecYear':monthWiseOfficeAmountBasedOnCompanyInSpecYear
     };
 })();
